ea837e50b79fbe9b309fb109406efc042ee35f2a
Matt Walker <matthew.walker@sky.uk>
2022-01-21 15:52:07 +0000

    Added script to create minified output

---
+++ web-components-vanilla/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+rm -rf dist/
+mkdir dist
+cp -r index.css assets dist/
+cp index.html dist/tmp.html
+cat **/*.js > dist/bundle.js
+minify dist/bundle.js > dist/bundle.min.js
+cat dist/tmp.html | sed "/script src/d" | sed 's/<!-- BEGIN-SCRIPTS-->/<script src="bundle.min.js"><\/script>/' \
+	> dist/index.html
